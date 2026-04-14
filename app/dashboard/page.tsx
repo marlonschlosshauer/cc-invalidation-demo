f676dad615e3b967e2476e491a2b06976f3fb4e9
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2026-04-14 15:53:00 +0200

    test 1

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,11 @@
 import { Invalidate } from "@/components/Invalidate";
 import { Posts } from "@/components/Posts";
-import { Suspense } from "react";
 
 export default function Dashboard() {
   return (
     <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
       Welcome to dashboard
-      <Suspense fallback={<p>Loading...</p>}>
-        <Posts />
-      </Suspense>
+      <Posts />
       <Invalidate />
     </main>
   );
